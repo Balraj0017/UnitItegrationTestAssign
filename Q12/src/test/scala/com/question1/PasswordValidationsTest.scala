3b3a9fcdccbd03f45ac31aa5bb863bf4dad11b28
balraj sabharwal <balraj.sabharwal@knoldus.com>
2021-12-05 16:03:41 +0530

    Balraj sabharwal

--- Q12/src/test/scala/com/question1/PasswordValidationsTest.scala
+++ Q12/src/test/scala/com/question1/PasswordValidationsTest.scala
@@ -4,37 +4,37 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class PasswordValidatorTest extends AnyFlatSpec {
   "password" should "be invalid as it contains space" in {
-    val password = new PasswordValidations("Kirti@ 1234")
+    val password = new PasswordValidations("chirag 2003")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as it does not contains any digit" in {
-    val password = new PasswordValidations("Rakhi#Pareek")
+    val password = new PasswordValidations("balraj#chirag")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as the length is less than 8" in {
-    val password = new PasswordValidations("Rakhi2$")
+    val password = new PasswordValidations("chirag5$")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as the length is more than 15" in {
-    val password = new PasswordValidations("Rakhi@pareek2000")
+    val password = new PasswordValidations("Balraj@chirag2003")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as it does not contains any lowercase letter" in {
-    val password = new PasswordValidations("RAKHI@PAREEK2")
+    val password = new PasswordValidations("BALRAJ@Chirag5")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as it does not contains any uppercase letter" in {
-    val password = new PasswordValidations("rakhi@pareek2")
+    val password = new PasswordValidations("balraj@chirag5")
     assert(!password.passwordIsValid)
   }
 
   "password" should "be invalid as it does not contains any special character" in {
-    val password = new PasswordValidations("RakhiPareek2")
+    val password = new PasswordValidations("Balrajchirag7")
     assert(!password.passwordIsValid)
   }
 
--- Q3/src/test/scala/com/knoldus/validator/EmailValidatorUnitTest.scala
+++ Q3/src/test/scala/com/knoldus/validator/EmailValidatorUnitTest.scala
@@ -5,27 +5,27 @@ import org.scalatest.flatspec.AnyFlatSpec
 class EmailValidatorUnitTest extends AnyFlatSpec {
   val emailValidator = new EmailValidator
   "email" should "be valid" in {
-    val result: Boolean = emailValidator.emailIdIsValid("humera@knoldus.com")
+    val result: Boolean = emailValidator.emailIdIsValid("chirag@knoldus.com")
     assert(result)
   }
 
   "email" should "be invalid if @ is not present there" in {
-    val result: Boolean = emailValidator.emailIdIsValid("abcknoldus.com")
+    val result: Boolean = emailValidator.emailIdIsValid("abbalraj.com")
     assert(!result)
   }
 
   "email" should "be invalid if it is Starting with Special Character'" in {
-    val result: Boolean = emailValidator.emailIdIsValid("%xyzknoldus.com")
+    val result: Boolean = emailValidator.emailIdIsValid("%jdkknoldus.com")
     assert(!result)
   }
 
   "email" should "be invalid if there is any space between it" in {
-    val result: Boolean = emailValidator.emailIdIsValid("abc @knoldus.com")
+    val result: Boolean = emailValidator.emailIdIsValid("jdk @knoldus.com")
     assert(!result)
   }
 
   "email" should "be invalid if it is not as per the rule'" in {
-    val result: Boolean = emailValidator.emailIdIsValid("abc@knoldus.xyz")
+    val result: Boolean = emailValidator.emailIdIsValid("chirag@knoldus.xyz")
     assert(!result)
   }
 
